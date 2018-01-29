96c14010e45b86cd0e14bf5ce2c6e3dd5216da73
Dawid Wysakowicz <dawid@getindata.com>
2018-01-29 16:55:10 +0100

    Working flink + http pod

--- flink-beam-poc/src/main/java/com/getindata/StreamingJob.java
+++ flink-beam-poc/src/main/java/com/getindata/StreamingJob.java
@@ -1,24 +1,22 @@
 package com.getindata;
 
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+import java.util.Random;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.metrics.MetricGroup;
+import org.apache.flink.runtime.checkpoint.CheckpointOptions;
+import org.apache.flink.runtime.checkpoint.OperatorSubtaskState;
+import org.apache.flink.runtime.jobgraph.OperatorID;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.graph.StreamConfig;
+import org.apache.flink.streaming.api.operators.ChainingStrategy;
+import org.apache.flink.streaming.api.operators.OneInputStreamOperator;
+import org.apache.flink.streaming.api.operators.OperatorSnapshotResult;
+import org.apache.flink.streaming.api.operators.Output;
+import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.streaming.runtime.streamrecord.LatencyMarker;
+import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
+import org.apache.flink.streaming.runtime.tasks.StreamTask;
 
 
 /**
@@ -26,11 +24,113 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
  */
 public class StreamingJob {
 
-	public static void main(String[] args) throws Exception {
-		// set up the streaming execution environment
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+  public static void main(String[] args) throws Exception {
+    // set up the streaming execution environment
+    final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		// execute program
-		env.execute("Flink Streaming Java API Skeleton");
-	}
+    env.addSource(new SourceFunction<Integer>() {
+
+      private boolean isRunning = false;
+
+      @Override
+      public void run(SourceContext<Integer> sourceContext) throws Exception {
+        final Random random = new Random();
+        while (isRunning) {
+          sourceContext.collect(random.nextInt());
+        }
+      }
+
+      @Override
+      public void cancel() {
+        isRunning = true;
+      }
+    }).transform("http", BasicTypeInfo.STRING_TYPE_INFO,
+        new OneInputStreamOperator<Integer, String>() {
+          @Override
+          public void processElement(StreamRecord<Integer> streamRecord) throws Exception {
+
+          }
+
+          @Override
+          public void processWatermark(Watermark watermark) throws Exception {
+
+          }
+
+          @Override
+          public void processLatencyMarker(LatencyMarker latencyMarker) throws Exception {
+
+          }
+
+          @Override
+          public void setup(StreamTask<?, ?> streamTask, StreamConfig streamConfig,
+              Output<StreamRecord<String>> output) {
+
+          }
+
+          @Override
+          public void open() throws Exception {
+
+          }
+
+          @Override
+          public void close() throws Exception {
+
+          }
+
+          @Override
+          public void dispose() throws Exception {
+
+          }
+
+          @Override
+          public OperatorSnapshotResult snapshotState(long l, long l1,
+              CheckpointOptions checkpointOptions)
+              throws Exception {
+            return null;
+          }
+
+          @Override
+          public void initializeState(OperatorSubtaskState operatorSubtaskState) throws Exception {
+
+          }
+
+          @Override
+          public void notifyOfCompletedCheckpoint(long l) throws Exception {
+
+          }
+
+          @Override
+          public void setKeyContextElement1(StreamRecord<?> streamRecord) throws Exception {
+
+          }
+
+          @Override
+          public void setKeyContextElement2(StreamRecord<?> streamRecord) throws Exception {
+
+          }
+
+          @Override
+          public ChainingStrategy getChainingStrategy() {
+            return null;
+          }
+
+          @Override
+          public void setChainingStrategy(ChainingStrategy chainingStrategy) {
+
+          }
+
+          @Override
+          public MetricGroup getMetricGroup() {
+            return null;
+          }
+
+          @Override
+          public OperatorID getOperatorID() {
+            return null;
+          }
+        });
+
+    // execute program
+    env.execute("Flink Streaming Java API Skeleton");
+  }
 }
